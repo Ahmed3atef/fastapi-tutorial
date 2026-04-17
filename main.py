6639cfb477bedd7a605ba58f4d96c699dc7d0aad
Ahmed@Atef <clay674302@gmail.com>
2026-04-17 17:53:09 +0200

    feat: initialize FastAPI application with root and post retrieval endpoints

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from fastapi-tutorial!")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+posts = [
+    {"title": "Post 1", "content": "Content 1"},
+    {"title": "Post 2", "content": "Content 2"},
+    {"title": "Post 3", "content": "Content 3"},
+]
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def root() -> dict[str, str]:
+    return "<h1>Hello World</h1>"
+
+@app.get("/api/posts")
+def get_posts() -> list[dict[str, str]]:
+    return posts
+
+@app.get("/api/posts/{id}")
+def get_one_post(id: int) -> dict[str, str]:
+    return posts[id-1]
