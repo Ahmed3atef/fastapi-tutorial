075b3a95180fe8be64cd3603f9932d1db8de9f05
Ahmed@Atef <clay674302@gmail.com>
2026-04-17 23:45:47 +0200

    feat: add home page template, static assets, and update main application routes

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-posts = [
-    {"title": "Post 1", "content": "Content 1"},
-    {"title": "Post 2", "content": "Content 2"},
-    {"title": "Post 3", "content": "Content 3"},
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-def root() -> dict[str, str]:
-    return "<h1>Hello World</h1>"
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def root(request: Request) -> HTMLResponse:
+    context_dict = {
+        "title": "Home",
+        "posts": posts
+    }
+    return templates.TemplateResponse(request, "home.html", context=context_dict)
 
 @app.get("/api/posts")
-def get_posts() -> list[dict[str, str]]:
+def get_posts() -> list[dict]:
     return posts
 
 @app.get("/api/posts/{id}")
-def get_one_post(id: int) -> dict[str, str]:
-    return posts[id-1]
+def get_one_post(id: int) -> dict:
+    for post in posts:
+        if post["id"] == id:
+            return post
+    return {"error": "Post not found"}
